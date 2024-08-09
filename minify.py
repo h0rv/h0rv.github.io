23a72dc2442c711e42c61b2f8eff05200621b4ca
Robby <h0rv@users.noreply.github.com>
2024-08-09 18:35:50 -0400

    fix minify

--- minify.py
+++ minify.py
@@ -10,23 +10,10 @@ def html(directory):
         with open(file_path, "r", encoding="utf-8") as file:
             html_content = file.read()
 
-        # full list of opts: https://github.com/wilsonzlin/minify-html/blob/master/minify-html-python/minify_html.pyi
         minified_html = minify_html.minify(
             html_content,
-            allow_noncompliant_unquoted_attribute_values=False,
-            allow_optimal_entities=False,
-            allow_removing_spaces_between_attributes=True,
-            keep_closing_tags=False,
-            keep_comments=False,
-            keep_html_and_head_opening_tags=False,
-            keep_input_type_text_attr=False,
-            keep_ssi_comments=False,
-            minify_css=True,
-            minify_doctype=False,
             minify_js=True,
-            preserve_brace_template_syntax=False,
-            preserve_chevron_percent_template_syntax=False,
-            remove_bangs=False,
+            minify_css=True,
             remove_processing_instructions=True,
         )
 
